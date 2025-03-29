786fe393a717751366c8527bc67ffe7adf197ebb
Aashutosh Sah <aashutoshsah0.1.1@gmail.com>
2025-03-30 05:08:44 +0545

    Update svm_email_classifier.py

--- svm/svm/svm_email_classifier.py
+++ svm/svm/svm_email_classifier.py
@@ -9,8 +9,6 @@ from sklearn.metrics import accuracy_score, classification_report
 
 # Load dataset
 df = pd.read_csv("spam_email_dataset.csv")
-
-
 # Preprocessing function
 def preprocess_text(text):
     text = text.lower()
